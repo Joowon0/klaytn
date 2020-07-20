799802d2156af23f50c1b18cb919257ae0d8c742
winnie.byun <winnie.byun@groundx.xyz>
2020-07-20 18:27:30 +0900

    clean test code

--- storage/database/dynamodb_test.go
+++ storage/database/dynamodb_test.go
@@ -13,20 +13,15 @@ func TestDynamoDB(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	//testRand := rand.New(rand.NewSource(time.Now().UnixNano()))
 	testKey := common.MakeRandomBytes(10)
 	testVal := common.MakeRandomBytes(100)
-	//testKey := randStrBytes(testRand.Intn(10))
-	//testVal := randStrBytes(5 * 10)
 
 	val, err := dynamo.Get(testKey)
-
 	assert.Nil(t, val)
 	assert.Error(t, err)
-	//assert.True(t, strings.Contains(err.Error(), "NoSuchKey"))
+	assert.Equal(t, err.Error(), dataNotFoundErr.Error())
 
 	assert.NoError(t, dynamo.Put(testKey, testVal))
-
 	returnedVal, returnedErr := dynamo.Get(testKey)
 	assert.Equal(t, testVal, returnedVal)
 	assert.NoError(t, returnedErr)
